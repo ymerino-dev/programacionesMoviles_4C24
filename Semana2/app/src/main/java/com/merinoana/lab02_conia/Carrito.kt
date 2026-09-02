43b811f0981acbc615fbdcdeea9e696a357541ee
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-02 12:16:22 -0500

    Agregar las funciones de calcularSubtotal,IGV y toal

--- Semana2/app/src/main/java/com/merinoana/lab02_conia/Carrito.kt
+++ Semana2/app/src/main/java/com/merinoana/lab02_conia/Carrito.kt
@@ -7,6 +7,23 @@ data class Producto(
     var cantidad: Int
 )
 
+// --- INICIO PARTE 3: Funciones de calculo ---
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    return subtotal * 0.18 // 18% del subtotal
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
+}
+
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
